c40b6d8729e6a1dc209cc4897dc8d195c7d245f7
ismol <ismo.loukusa@gmail.com>
2016-02-15 14:53:47 +0200

    Added api call for delete

--- app/routes.js
+++ app/routes.js
@@ -3,20 +3,18 @@
  */
 
 // app/routes.js
-
+//JSON.stringify( response.data, null, 2)
 var Customer = require('./models/customer');
 
 var Order = require('./models/order');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get('/api/customers', function(req, res) {
+    app.get('/api/customers', function (req, res) {
 
         // use mongoose to get all customers in the database
-        Customer.find(function(err, customers) {
+        Customer.find(function (err, customers) {
 
-            // if there is an error retrieving, send the error.
-            // nothing after res.send(err) will execute
             if (err)
                 res.send(err);
 
@@ -24,9 +22,58 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/api/orders', function(req, res) {
+    app.post('/api/customers', function (req, res) {
 
-        Order.find(function(err, orders) {
+
+        //console.log("req.body: " + JSON.stringify(req.body, null, 2));
+
+        //var customer = JSON.parse ( "[
+        //{
+        //    "firstname": "Firstname",
+        //    "lastname": "Lastname",
+        //    "city": "City"
+        //}
+        //]");
+
+        var customer = JSON.parse(req.body);
+
+        console.log("customer.firstname: " + customer.firstname);
+        console.log("customer.lastname: " + customer.lastname);
+        console.log("customer.city: " + customer.city);
+
+
+
+        var newCustomer = new Customer();
+        newCustomer.firstname = req.body.firstname;
+        newCustomer.lastname = req.body.lastname;
+        newCustomer.city = req.body.city;
+
+        //console.log("newCustomer.firstname: " + newCustomer.firstname);
+        //console.log("newCustomer.lastname: " + newCustomer.lastname);
+        //console.log("newCustomer.city: " + newCustomer.city);
+
+        //save the customer
+        //newCustomer.save(function (err) {
+        //    if (err) res.send(err);
+        //
+        //    res.send("success");
+        //});
+
+    });
+
+    app.delete('/api/customers/:customerId', function (req, res) {
+
+        Customer.remove({_id: req.params.customerId}, function (err, Customer) {
+            if (err)
+                res.send(err);
+
+            res.json({message: 'Successfully deleted'});
+        });
+    });
+
+    app.get('/api/orders', function (req, res) {
+
+        Order.find(function (err, orders) {
             if (err)
                 res.send(err);
 
@@ -34,13 +81,7 @@ module.exports = function(app) {
         });
     });
 
-    // route to handle creating goes here (app.post)
-    // route to handle delete goes here (app.delete)
-
-    // frontend routes =========================================================
-    // route to handle all angular requests
-
-    app.get('*', function(req, res) {
+    app.get('*', function (req, res) {
         res.sendfile('./public/views/index.html');
     });
 
