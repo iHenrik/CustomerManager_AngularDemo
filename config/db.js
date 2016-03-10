ad973dbbc2c9bb1ae29eb7c4100c38387d99b8fa
ismol <ismo.loukusa@gmail.com>
2016-03-10 20:39:15 +0200

    Changed db connection from local into MongoLab cloud

--- config/db.js
+++ config/db.js
@@ -2,5 +2,7 @@
  * Created by ismol on 09/02/2016.
  */
 module.exports = {
-    url : 'mongodb://localhost:27017/customers'
+    url : 'mongodb://meanuser:meanpass@ds039185.mlab.com:39185/mean-training'
 }
+
+//'mongodb://localhost:27017/customers'
